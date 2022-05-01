50d1b59afdd5a7382bde1c00d744b75825114009
Yash Shelar <94430653+Yashgith@users.noreply.github.com>
2022-05-01 15:24:21 +0530

    Add files via upload

--- yash.js
+++ yash.js
@@ -19,3 +19,34 @@ const searchmovies=()=>{
         }
     }
     }
+
+
+    var btn=document.getElementById('submit')
+    btn.addEventListener('click',function(e){
+      e.preventDefault()
+      var name=document.getElementById('name').value;
+      var email=document.getElementById('email').value;
+      var phno=document.getElementById('phno').value;
+      var body ='name :'+name+'<br/> email:'+email+'<br/> phone:'+phno;
+      
+      Email.send({
+      Host : "smtp.gmail.com",
+      Username : "yashshelar1122@gmail.com",
+      Password : "fxjxzbejqswozbqq",
+      To : 'yashshelar1122@gmail.com',
+      From : email,
+      Subject : "Sign up",
+      Body : body+"You successfully Sign up in our Movie Mania Website."
+  }).then(
+      message=>{
+          alert("Successful")
+          if(message=='OK'){
+              window.location.replace("yash.html")
+          }
+  
+          else{
+              console.error(message)
+          }
+      }
+  );
+    })
